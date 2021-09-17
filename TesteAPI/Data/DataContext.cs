e4f4f0ea0ecbc5c60db258aced87100fecc5346a
Moises <Moises@PC-CASA>
2021-09-17 10:51:10 -0300

    Adicionar arquivos de projeto.

---
+++ TesteAPI/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteAPI.Models;
+
+namespace TesteAPI.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+
+    }
+}
